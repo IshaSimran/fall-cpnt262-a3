9497e7770f241a68e07584321d955a12e669917c
Isha Sidhu <ishasimranxo@gmail.com>
2021-10-25 21:32:30 -0600

    changes made

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Fall-CPNT262-A3 : Fetching API Data
 ## By Isha Simran
 
-- [GitHub Repo]()
+- [GitHub Repo](https://github.com/IshaSimran/fall-cpnt262-a3)
 
 - [GH Pages Site]()
 
 
 ## Attributions
 
-### Icons
+### API
+
+- [Anime Facts Rest Api](https://chandan-02.github.io/anime-facts-rest-api/) Created by Chandan-02 on GitHub
 
 ### Fonts
---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+fetch('https://anime-facts-rest-api.herokuapp.com/api/v1/demon_slayer')
+  .then(function (response) {
+    // JSON that is returned from the server must be converted to a JS object asynchronously.
+    if (!response.ok) {
+      throw new Error('Not 200 OK');
+    }
+    return response.json();
+  })
+  .then(function (data) {
+    // Any code that depends on the `data` must go in this block
+    const animeFacts = document.querySelector('.facts');
+
+    animeFacts.innerHTML = `
+      <figure>
+       <img src="${data.url}" alt="Random Anime Fact">
+       <figcaption>${data.fileSizeBytes} bytes of data.</figcaption>
+      </figure>
+    `;
+    console.log(data);
+  })
+  .catch(function (err) {
+    // An error or `reject` from any of the above `.then()` blocks will end up here.
+    console.log(err);
+  });
+
