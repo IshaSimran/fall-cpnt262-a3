69a166c38173b9fb0872c7dc1a0b5770f5707fc6
Isha Sidhu <ishasimranxo@gmail.com>
2021-10-28 19:10:51 -0600

    changes made

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Fall-CPNT262-A3 : Fetching API Data
 ## By Isha Simran
 
+I just want to clarify that this assignment broke me more than I broke it. And the fact that I got an image to load is good enough for me.
+
 - [GitHub Repo](https://github.com/IshaSimran/fall-cpnt262-a3)
 
 - [GH Pages Site]()
@@ -9,10 +11,13 @@
 
 ### API
 
-- [Anime Facts Rest Api](https://chandan-02.github.io/anime-facts-rest-api/) Created by Chandan-02 on GitHub
+- [API Link](https://api.unsplash.com/search/photos?query=food&client_id=z-F7xvnBY_0FDRI3KtFPnSOsS3k8Ruo00avGnxxQv-o)
 
-- [API Link](https://anime-facts-rest-api.herokuapp.com/api/v1/demon_slayer)
+- [Unsplash Developers](https://unsplash.com/developers)
+
+- [Unsplash Developers API Terms](https://unsplash.com/api-terms)
 
 ### Fonts
 
 - [Karla Font]() Designed by Johnny Pinhorn
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,29 @@
-// API Array
-const newAnime = function () {
+// Strict Mode
+'use strict';
 
-  const object = [
-    `1`,
-    `2`,
-    `3`,
-    `4`,
-    `5`,
-    `6`,
-    '7',
-    '8',
-    `9`,
-    `10`,
-    `11`,
-    `12`,
-    `13`,
-  ]
+// Variables Declared
+const url =
+  'https://api.unsplash.com/search/photos?query=food&client_id=z-F7xvnBY_0FDRI3KtFPnSOsS3k8Ruo00avGnxxQv-o';
+const button = document.querySelector('.button');
+const display = document.querySelector('.display');
 
-  // Loop to display array
-  const randAnime = object[Math.floor(Math.random() * object.length)];
+// Button Added
+button.addEventListener('click', async () => {
+  let randomImage = await getNewImage();
+  display.src = randomImage;
+});
 
-  fetch('https://anime-facts-rest-api.herokuapp.com/api/v1')
-    .then(function (response) {
-      // JSON that is returned from the server must be converted to a JS object asynchronously.
-      if (!response.ok) {
-        throw new Error('Not 200 OK');
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      // Any code that depends on the `data` must go in this block
-      const animeObject = document.querySelector('.randAnime');
-
-      animeObject.innerHTML = `
-      <figure>
-       <img src="${data.anime_img}" alt="Random Anime">
-       <figcaption>${data.anime_name}</figcaption>
-      </figure>
-    `;
-      console.log(data);
+// Fetch Sequence
+async function getNewImage() {
+  let randomNumber = Math.floor(Math.random() * 10);
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let allImages = data.results[randomNumber];
+      return allImages.urls.regular;
     })
     .catch(function (err) {
       // An error or `reject` from any of the above `.then()` blocks will end up here.
       console.log(err);
     });
 }
-
-// Event listener for button
-document.querySelector('.different-anime').addEventListener('click', newAnime);
